1609a6f487de75725748b6f90611f380ef22eba6
Mitradigitalindonesia <mitradigitalindonesia99@gmail.com>
2025-05-31 15:59:30 +0700

    Delete create_tables.py

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-# create_tables.py
-from database import engine, Base
-from models import TradeHistory, PortfolioSnapshot
-
-# Membuat semua tabel dari model
-Base.metadata.create_all(bind=engine)
-print("✅ Semua tabel berhasil dibuat.")
